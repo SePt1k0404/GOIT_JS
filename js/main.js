e8a8850f22befff7104d22987bb70f9d1b052bdd
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-05-10 21:43:29 +0300

    test 1

---
+++ js/main.js
@@ -0,0 +1,90 @@
+//_____________________заняття 1_________________________
+
+// Напишіть інструкцию, яка приймає рядок як аргумент і перетворює регістр першого
+// символу рядка з нижнього регістру у верхній.
+//const str = "string not starting with capital";
+
+// const str = "string not starting with capital";
+
+// const value = str.replace('s', "S");
+
+// console.log(value);
+
+// Переробіть наведений код так, щоб у ньому використовувалися операції +=, -=, *=, /=, ++, --.
+//Кількість рядків коду не повинна змінитися. Код для переробки:
+// let num = 1;
+// num = num + 12;
+// num = num - 14;
+// num = num * 5;
+// num = num / 7;
+// num = num + 1;
+// num = num - 1;
+
+// let num = 1;
+// num += 12;
+// num -= 14;
+// num *= 5;
+// num /= 7;
+// num += 1;
+// num -= 1;
+
+
+//Шаблонные строки
+//Написать фразу с помощью конкатенации и шаблонных строк
+//"Hello, my name is [name], I'm [age] years old and I like [hobby]", где в квадратных скобках - переменные вставленные в строку
+
+
+// const name = 'Dan';
+// const age = 35;
+// const hobby = 'swimming';
+
+// const name = 'Dan';
+// const age = 35;
+// const hobby = 'swimming';
+
+// let message = `Hello, my name is ${name}, I'm ${age} years old and I like ${hobby}`;
+// console.log(message);
+
+
+// message = "Hello, my name is " + name + ", I'm " + age + " years old and I like " + hobby;
+// console.log(message);
+
+
+// Підрахуйте суму чисел у діапазоні від 1 до 10 за допомогою циклу
+// for, while та do...while.
+// Підказка: у результаті має вийти 55.
+
+// let result = 0;
+
+// for (let i = 1; i <= 10; i += 1 ){
+//     result += i;
+// }
+
+// let i = 1;
+// while (i !== 11) {
+//     result += i;
+//     i += 1;
+// }
+
+// let i = 0;
+// do {
+//     result += i;
+//     i += 1;
+// }while(i <= 10)
+
+// console.log(result);
+
+//_____________________/заняття 1_________________________
+
+// let link = "https://my-site.com/about";
+
+// link += !link.endsWith("/") && link.includes('my-site') ? '/' : '';
+
+// console.log(link);
+
+
+// const arr = ['8', '11'];
+
+// console.log(arr);
+
+// console.log((Number(arr[0]) + Number(arr[1])) * 2);
